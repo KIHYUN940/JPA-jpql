bea8603be47070f29b80cb442054b2b6bece51a9
kihyun940 <houng027@gmail.com>
2024-03-14 03:40:26 +0900

    Feat: JPQL - 기본 문법과 쿼리 API
    JPQL은 객체지향 쿼리 언어다.따라서 테이블을 대상으로 쿼리 하는 것이 아니라 엔티티 객체를 대상으로 쿼리한다.
    - JPQL은 결국 SQL로 변환
    
    JPQL 문법
    • select m from Member as m where m.age > 18
    • 엔티티와 속성은 대소문자 구분O (Member, age)
    • JPQL 키워드는 대소문자 구분X (SELECT, FROM, where)
    • 엔티티 이름 사용, 테이블 이름이 아님(Member)
    • 별칭은 필수(m) (as는 생략가능)
    
    TypeQuery, Query
    - TypeQuery: 반환 타입이 명확할 때 사용
    - Query: 반환 타입이 명확하지 않을 때 사용
    
    TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); //타입 정보를 받을 수 있을 때 TypeQuery 사용
    Query query3 = em.createQuery("select m.username, m.age from Member m"); //타입 정보를 받을 수 없을 때 Query 사용
    
    결과 조회 API
    - query.getResultList(): 결과가 하나 이상일 때, 리스트 반환
    -> 결과가 없으면 빈 리스트 반환 -> NullPointerException 걱정 no
    
    - query.getSingleResult(): 결과가 정확히 하나, 단일 객체 반환
    -> 결과가 없으면: javax.persistence.NoResultException
    -> 둘 이상이면: javax.persistence.NonUniqueResultException
    
    파라미터 바인딩
    Member result3 = em.createQuery("select m from Member m where m.username = :username", Member.class)
                        .setParameter("username", "member1")
                        .getSingleResult();
    System.out.println("result3 = " + result3.getUsername());

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -23,6 +23,31 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class); //타입 정보를 받을 수 있을 때 TypeQuery 사용
+            Query query3 = em.createQuery("select m.username, m.age from Member m"); //타입 정보를 받을 수 없을 때 Query 사용
+
+            List<Member> resultList = query1.getResultList(); //값이 여러 개 반환될 때 / 결과가 없으면 빈 리스트 반환 -> NullPointerException 걱정 no
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            Member result = query1.getSingleResult();//값이 하나만 반환될 때 / 결과가 정확하게 하나가 나와야 함 -> 결과가 없으면 NoResultException, 둘 이상이면 NonUniqueResultException
+            System.out.println("result = " + result);
+
+
+            //파라미터 바인딩
+//            query1.setParameter("username", "member1");
+//            Member result2 = query1.getSingleResult();
+//            System.out.println("result2 = " + result2.getUsername());
+
+            ///보통 체이닝을 해서 사용 - 이름 기준, 위치 기준 - 위치 기준은 사용 X
+            Member result3 = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result3 = " + result3.getUsername());
+
+
             System.out.println("=======");
 
             tx.commit();
